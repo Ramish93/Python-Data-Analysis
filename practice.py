51572908029ecdefda807749fe3a5df1f0067384
Ramish Hassan <ramishshah93@gmail.com>
2022-08-24 15:09:52 +0200

    inital commit, practice file and main file included

---
+++ practice.py
@@ -0,0 +1,24 @@
+# practice with data and graphs
+
+# import pandas
+
+# data = pandas.read_csv('reviews.csv')
+
+# print(data.hist('Rating'))
+# print(data['Rating'].mean())
+
+# print(data['Course Name', 'Rating'])
+
+# print(data.iloc[3])
+# print(data.iloc[1:3])
+
+
+# print(data.at[2, 'Rating'])
+
+# d2 = data[data['Rating'] > 4]
+# print(data['Rating'].mean())
+# print(data[data['Comment'].isnull()]['Rating'].mean())
+# print(data[data['Comment'].isnull()]['Rating'].count())
+# print(data[data['Comment'].notnull()]['Rating'].count())
+
+# print(data[data['Comment'].str.contains('accent', na=False)])
